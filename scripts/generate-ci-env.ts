c3f08fd0c0a17ffc03fd288e7070d6466c631a96
Charles Lien <charleslien97@gmail.com>
2025-10-28 19:11:01 -0700

    switch logging to console error

--- scripts/generate-ci-env.ts
+++ scripts/generate-ci-env.ts
@@ -73,6 +73,6 @@ function generateGitHubEnv() {
   console.log(JSON.stringify(selected))
 }
 
-console.log({ asdfenv: process.env }, 'asdf')
+console.error({ asdfenv: process.env }, 'asdf')
 
 generateGitHubEnv()
